3211cf08bf494cd40c90f0ff397c33eb96fe398f
Jay Nguyen <nguyen.le.kim.phuc@gmail.com>
2020-08-03 15:59:01 +1000

    Prettify the HTML and fix some issues.

--- check_user.php
+++ check_user.php
@@ -3,7 +3,7 @@
 session_start();
 
 // Check if the user is already logged in, if yes then redirect him to HOME Page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     header("location: home.php");
     exit;
 }
@@ -19,8 +19,8 @@ $password_err = "";
 
 // Store Form Data
 //if(isset($GET["email"]) && isset($GET["password"])){
-$email = $_POST["email"];
-$password = $_POST["password"];
+$email = array_key_exists('email', $_POST) ? $_POST["email"] : null;
+$password = array_key_exists('password', $_POST) ? $_POST["password"] : null;
 
 // Validate credentials
 // Prepare a select statement
@@ -29,7 +29,8 @@ $sql = "SELECT * FROM users where email='$email'";
 $result = mysqli_query($link, $sql); // $link is obtained
 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 $count = mysqli_num_rows($result);
-if($count > 1){
+
+if ($count > 0) {
     // Password is correct, so start a new session
     session_start();
 
@@ -41,7 +42,7 @@ if($count > 1){
 
     // Redirect user to welcome page
     header("location: home.php");
-} else{
+} else {
     // Display an error message if password is not valid
     $email_err = "No account found with that email.";
     $password_err = "The password you entered was not valid.";
@@ -51,55 +52,76 @@ if($count > 1){
 // Close connection
 mysqli_close($link);
 
-if($email_err !="" || $password_err !=""){
+if ($email_err !="" || $password_err !="") {
     ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>User Login</title>
-        <link rel="stylesheet"
-              href=https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css >
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <title>INTE1070</title>
+
+        <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+        <link rel="stylesheet" href="assets/custom.css" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <style>
-            .page-style{
-                margin: 20px;
-            }
-        </style>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/fontawesome.min.js"></script>
     </head>
     <body>
-    <div class="page-style">
-        <h2>Welcome to E-Commerce Site</h2>
+    <div class="inte-header">
+        <h2>INTE1070: Secure Electronic Commerce</h2>
     </div>
-    <div class="page-style">
-        <h4>User Login</h4>
-    </div>
-    <div class="page-style">
-        <h4>Error Occured!</h4>
-    </div>
-    <div class="page-style">
-        <h4><?php echo $email_err;?></h4>
-    </div>
-    <div class="page-style">
-        <h4><?php echo $password_err;?></h4>
-    </div>
-    <div class="page-style">
-        <form action="check_user.php" method="post">
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="text" class="form-control" id="email" placeholder="Enter your registered Email">
+
+    <div class="container">
+        <div class="login-area">
+            <h4>Welcome to E-commerce website</h4>
+
+            <div class="login-error">
+                <p><?php echo $email_err;?></p>
+                <p><?php echo $password_err;?></p>
             </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Enter your Password">
+
+            <div class="login-row">
+                <form action="./check_user.php" method="post">
+                    <div class="col s12" style="margin-bottom: 7px;">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fas fa-at" style="font-size: 25px"></i>
+                            </span>
+                                </div>
+                                <input name="email" value="<?php echo $email; ?>" type="text" class="form-control" placeholder="Email">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12" style="margin-bottom: 7px;">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fas fa-key" style="font-size: 25px"></i>
+                                </span>
+                                </div>
+                                <input name="password" value="<?php echo $password; ?>" type="text" class="form-control" placeholder="Password">
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fas fa-sign-in-alt"></i> Login
+                    </button>
+                </form>
             </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+        </div>
     </div>
+
+    <div class="footer">S3493188 Le Kim Phuc Nguyen</div>
     </body>
     </html>
     <?php
--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 session_start();
 
 // Check if the user is logged in, if not then redirect him to login page (index.html)
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: index.html");
     exit;
 }
@@ -13,30 +13,34 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>User Login</title>
-    <link rel="stylesheet"
-          href=https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css >
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <title>INTE1070</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+    <link rel="stylesheet" href="assets/custom.css" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <style>
-        .page-style{
-            margin: 20px;
-        }
-    </style>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/fontawesome.min.js"></script>
 </head>
 <body>
-<div class="page-header">
-    <h4>Hi, <?php echo $_SESSION["first_name"]." ".$_SESSION["last_name"]; ?></h4>
-</div>
-<div class="page-header">
-    <h4>Welcome to our E-Commerce Site.</h4>
-</div>
-<div class="container">
-</div>
-<p>
-    <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-</p>
+    <div class="inte-header">
+        <h2>INTE1070: Secure Electronic Commerce</h2>
+    </div>
+
+    <div class="container home-hi">
+        <h2>Hi, <?php echo $_SESSION["first_name"]." ".$_SESSION["last_name"]; ?></h2>
+        <h4>Welcome to our E-Commerce Site.</h4>
+
+        <a href="logout.php" class="btn btn-danger">
+            <i class="fas fa-sign-out-alt"></i> Sign Out
+        </a>
+    </div>
+
+    <div class="footer">S3493188 Le Kim Phuc Nguyen</div>
 </body>
 </html>
